e474cd2a7a8ae2a1b2e53b549f610755fb6dfa30
baselrabia <baselrabia@gmail.com>
2023-03-20 15:40:40 +0200

    send email once by pushing a job to the db queue

--- app/Repositories/IngredientRepository.php
+++ app/Repositories/IngredientRepository.php
@@ -9,28 +9,28 @@ use Illuminate\Support\Facades\Mail;
 
 class IngredientRepository
 {
-        public function updateIngredientStock(int|string $ingredient_id, mixed $quantity): void
-        {
-            $ingredient = Ingredient::find($ingredient_id);
-            if (!$ingredient) {
-                throw new \Exception("this Ingredient is not exist with id " . $ingredient_id);
-            }
-
-            if ($ingredient->stock < $quantity) {
-                Log::error('updateIngredientStock() for ' . $ingredient->name .
-                    ' Quantity needed : ' . $quantity . ' Stock is : ' . $ingredient->stock);
-                throw new \Exception("The current ingredient stock is insufficient to fulfill the order quantity");
-            }
-            $ingredient->stock -= $quantity;
-            $ingredient->save();
-
-            $ingredient_threshold = $ingredient->full_stock * $ingredient->threshold;
-            if ($ingredient->stock < $ingredient_threshold && !$ingredient->notification_sent) {
-                // send email to the merchant
-                Mail::to('merchent@example.com')->queue(new IngredientThresholdReached($ingredient));
-                $ingredient->notification_sent = true;
-                $ingredient->save();
-            }
+    public function updateIngredientStock(int|string $ingredient_id, mixed $quantity): void
+    {
+        $ingredient = Ingredient::find($ingredient_id);
+        if (!$ingredient) {
+            throw new \Exception("this Ingredient is not exist with id " . $ingredient_id);
         }
+
+        if ($ingredient->stock < $quantity) {
+            Log::error('updateIngredientStock() for ' . $ingredient->name .
+                ' Quantity needed : ' . $quantity . ' Stock is : ' . $ingredient->stock);
+            throw new \Exception("The current ingredient stock is insufficient to fulfill the order quantity");
+        }
+        $ingredient->stock -= $quantity;
+        $ingredient->save();
+
+        $ingredient_threshold = ($ingredient->full_stock * $ingredient->threshold) / 100;
+        if ($ingredient->stock < $ingredient_threshold && !$ingredient->notification_sent) {
+            // send email to the merchant
+            Mail::to('merchent@example.com')->queue(new IngredientThresholdReached($ingredient));
+            $ingredient->notification_sent = true;
+            $ingredient->save();
+        }
+    }
 }
 
