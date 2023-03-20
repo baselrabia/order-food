4151a5ae8d23010327b8e85dd3cf82a9030590e3
baselrabia <baselrabia@gmail.com>
2023-03-20 15:45:44 +0200

    remove timestamp from order_product

--- database/migrations/2023_03_18_131808_create_order_product_table.php
+++ database/migrations/2023_03_18_131808_create_order_product_table.php
@@ -12,13 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('order_product', function (Blueprint $table) {
-            $table->id();
             $table->unsignedBigInteger('order_id');
             $table->unsignedBigInteger('product_id');
             $table->integer('quantity');
             $table->decimal('price', 8, 2);
             $table->decimal('subtotal', 8, 2);
-            $table->timestamps();
 
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
